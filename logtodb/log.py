033efb395de968090c769bb4a3758f47383a7ebd
Alex Ladda <alex.ladda@gmail.com>
2021-06-07 16:23:57 +0200

    def watch(logfile):

--- logtodb/log.py
+++ logtodb/log.py
@@ -1,3 +1,24 @@
 # <open nginx (combined) log and read new lines>
 # <parse lines>
 # <store them in database>
+
+import time
+import sys
+
+
+def watch(logfile):
+    fp = open(logfile, 'r')
+    while True:
+        newline = fp.readline()
+        # Once all lines are read this just returns ''
+        # until the file changes and a new line appears
+
+        if newline:
+            print(newline)
+        else:
+            time.sleep(0.5)
+
+
+logfile = sys.argv[1]
+
+watch(logfile)
