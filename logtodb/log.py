597fc1c37231653bf1fc7a01313b974f2e252424
Alex Ladda <alex.ladda@gmail.com>
2021-06-07 21:07:32 +0000

    added db reset

--- logtodb/log.py
+++ logtodb/log.py
@@ -3,9 +3,18 @@ import sys
 import re
 import sqlite3
 
+# specify which log file to analyse with command line argument
+LOGFILE = sys.argv[1]
+# setting sqlite db file
+DB_FILE = 'access_log.db'
+# regex for nginx combined log format
+LINEFORMAT = re.compile(
+    r"""(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?P<dateandtime>\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] ((\"(GET|POST) )(?P<url>.+)(http\/1\.1")) (?P<statuscode>\d{3}) (?P<bytessent>\d+) (["](?P<refferer>(\-)|(.+))["]) (["](?P<useragent>.+)["])""", re.IGNORECASE)
 
-def watch(logfile):
-    filepoll = open(logfile, 'r')
+
+def watch(LOGFILE):
+    # main loop to monitor the log file
+    filepoll = open(LOGFILE, 'r')
     while True:
         newline = filepoll.readline()
         # first this will read all lines all lines and return them
@@ -13,31 +22,22 @@ def watch(logfile):
         # then if a new line apperears, it will return that line
 
         if newline:
-            currentline = re.search(lineformat, newline)
-            data = currentline.groupdict()
-            print("newline found")
-            print("=============")
-            print("writing to db: ", list(data.values()))
-            write(data)
-            print("             ")
+            currentline = re.search(LINEFORMAT, newline)
+            if currentline:
+                data = currentline.groupdict()
+                print("log appended, writing to db:")
+                print("============================")
+                for item in data:
+                    print(item, data[item], sep=": ")
+                write(data)
+                print(" ")
         else:
             time.sleep(0.5)
 
 
 def write(data):
-    # try:
-    conn = sqlite3.connect('access_log.db')
-
-    conn.execute("""CREATE TABLE IF NOT EXISTS access_log (
-                    ipaddress TEXT,
-                    datetime TEXT,
-                    url TEXT,
-                    statuscode INTEGER,
-                    bytessent INTEGER,
-                    refferer TEXT,
-                    useragent TEXT
-                    )""")
-
+    # commiting data to the database
+    conn = sqlite3.connect(DB_FILE)
     conn.execute("""INSERT INTO access_log VALUES (
                     ?,
                     ?,
@@ -48,14 +48,24 @@ def write(data):
                     ?)""",
                  list(data.values()))
     conn.commit()
-    # except:
-    #     print("Unable to connect to the database")
-    #     quit()
 
 
-logfile = sys.argv[1]
-lineformat = re.compile(
-    r"""(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?P<dateandtime>\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] ((\"(GET|POST) )(?P<url>.+)(http\/1\.1")) (?P<statuscode>\d{3}) (?P<bytessent>\d+) (["](?P<refferer>(\-)|(.+))["]) (["](?P<useragent>.+)["])""", re.IGNORECASE)
+def reset_database():
+    # resetting database fresh every time the script is started
+    conn = sqlite3.connect(DB_FILE)
+    conn.execute("""DROP TABLE IF EXISTS access_log""")
+    conn.execute("""CREATE TABLE access_log (
+                    ipaddress TEXT,
+                    datetime TEXT,
+                    url TEXT,
+                    statuscode INTEGER,
+                    bytessent INTEGER,
+                    refferer TEXT,
+                    useragent TEXT
+                    )""")
+    conn.commit()
 
 
-watch(logfile)
+reset_database()
+
+watch(LOGFILE)
--- logtodb/stats.py
+++ logtodb/stats.py
@@ -1,11 +1,11 @@
 import sqlite3
 
-conn = sqlite3.connect('access_log.db')
+DB_FILE = 'access_log.db'
 
+conn = sqlite3.connect(DB_FILE)
 r = conn.execute("""SELECT statuscode, COUNT()
                 FROM access_log
                 GROUP BY statuscode;""")
-
 output = r.fetchall()
 
 for statuscode in output:
