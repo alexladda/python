b7ca44c7aa91277b2bf19dc599d64d5001ed62b7
Alex Ladda <alex.ladda@gmail.com>
2021-06-07 16:40:45 +0200

    regex

--- logtodb/log.py
+++ logtodb/log.py
@@ -14,11 +14,15 @@ def watch(logfile):
         # until the file changes and a new line appears
 
         if newline:
-            print(newline)
+            data = re.search(lineformat, newline)
+            print(data)
         else:
             time.sleep(0.5)
 
 
 logfile = sys.argv[1]
+lineformat = re.compile(
+    r"""(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?P<dateandtime>\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] ((\"(GET|POST) )(?P<url>.+)(http\/1\.1")) (?P<statuscode>\d{3}) (?P<bytessent>\d+) (["](?P<refferer>(\-)|(.+))["]) (["](?P<useragent>.+)["])""", re.IGNORECASE)
+
 
 watch(logfile)
